13872e10dcbf2ae04fd42cedfd9b031b2a823970
藍+85CD <50108258+kwaa@users.noreply.github.com>
2023-06-24 00:12:48 +0800

    feat(build): ✨ use sagernet fork

--- build.sh
+++ build.sh
@@ -15,7 +15,7 @@ else
         --with github.com/mholt/caddy-l4 \
         --with github.com/mholt/caddy-webdav \
         --with github.com/lindenlab/caddy-s3-proxy \
-        --with github.com/caddyserver/forwardproxy@caddy2=github.com/klzgrad/forwardproxy@naive \
+        --with github.com/caddyserver/forwardproxy@caddy2=github.com/kwaa/forwardproxy@naive \
         --with github.com/kwaa/caddy2-proxyprotocol@master \
         --output caddy_${arch}
         git add caddy_${arch}
